0caf69b107739dcc5e2b5cede8ee1859e54e106d
HenryNerd <henry.veedahl@outlook.com>
2024-08-26 17:17:36 -0500

    Number Guessing Part 1

---
+++ Number Guessing.py
@@ -0,0 +1,9 @@
+import random
+
+guesses = 3
+number = 0
+guess = 1
+
+print("  _____                 _ _   _                 ")
+print(" |  __ \               | | \ | |                ")
+print(" | |__) |__ _ _ __   __| |  \| |_   _ _ __ ___  ")
