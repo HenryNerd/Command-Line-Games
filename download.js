c4e236d75bcd197c8371a6b90c43d3dca0afc1f2
HenryNerd <henry.veedahl@outlook.com>
2024-08-28 12:47:50 -0500

    Download Website Part 2

--- download.js
+++ download.js
@@ -1,3 +1,11 @@
 document.getElementById('Luancher').addEventListener('click', function() {
     window.open('https://github.com/HenryNerd/Command-Line-Games/blob/main/main.py', '_blank');
 });
+
+document.getElementById('Harry Potter Duel Game').addEventListener('click', function() {
+    window.open('https://github.com/HenryNerd/Command-Line-Games/blob/main/HarryPotter.py', '_blank');
+});
+
+document.getElementById('Random Number Game').addEventListener('click', function() {
+    window.open('https://github.com/HenryNerd/Command-Line-Games/blob/main/NumberGuessing.py', '_blank');
+});
