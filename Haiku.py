e38e9ce252879162e4644d22deb997bdf06c3717
HenryNerd <henry.veedahl@outlook.com>
2024-08-28 10:03:29 -0500

    Haiku Generator Part 2

--- Haiku.py
+++ Haiku.py
@@ -1,6 +1,18 @@
 from openai import OpenAI
 
 client = OpenAI(
-  organization='YOUR_ORG_ID',
-  project='$PROJECT_ID',
+    api_key='',
+    organization='',
+    project='',
 )
+option1 = input("Item 1")
+option1 = input("Item 2")
+stream = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Give me a hiaku about",option1,"And"}],
+    stream=True,
+)
+
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
