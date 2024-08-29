9aa9d35a0802992b60228b020be4073fc8a0dd15
Henry <89225651+HenryNerd@users.noreply.github.com>
2024-08-29 11:48:03 -0500

    Add files via upload

--- Rock,Paper,Scissors.py
+++ Rock,Paper,Scissors.py
@@ -51,6 +51,7 @@ if int(gamemode) == 1:
         elif int(player2Choice) == 2:
             result = 2
 
+    os.system('cls')
     if int(player2Choice) == 1:
         print("Your PC chose Rock")
     elif int(player2Choice) == 2:
@@ -58,10 +59,112 @@ if int(gamemode) == 1:
     elif int(player2Choice) == 2:
         print("Your PC chose Scissors")
 
-if int(result) == 1:
-    print("Lose")
-elif int(result) == 2:
-    print("Tie")
-elif int(result) == 3:
-    print("Win")
-print("Game Over")
+    if int(result) == 1:
+        print("     __")
+        print("  _ / /")
+        print(" (_) | ")
+        print("   | | ")
+        print("  _| | ")
+        print(" (_) | ")
+        print("    \_\ ")
+        print("")
+        print("Sorry, You Lost")
+    elif int(result) == 2:
+        print("  ______ ")
+        print(" |______|")
+        print("  ______ ")
+        print(" |______|")
+        print("")
+        print("That was a tie")
+    elif int(result) == 3:
+        print(" __     __          __          ___       ")
+        print(" \ \   / /          \ \        / (_)      ")
+        print("  \ \_/ /__  _   _   \ \  /\  / / _ _ __  ")
+        print("   \   / _ \| | | |   \ \/  \/ / | | '_ \ ")
+        print("    | | (_) | |_| |    \  /\  /  | | | | |")
+        print("    |_|\___/ \__,_|     \/  \/   |_|_| |_|")
+    
+elif int(gamemode) == 2:
+    os.system('cls')
+    print("Player VS Player")
+    print("Player 1")
+    print("")
+    print("1. Rock")
+    print("2. Paper")
+    print("3. Scissors")
+    player1Choice = input("What would you like to chose ")
+    os.system('cls')
+    print("Player VS Player")
+    print("Player 2")
+    print("")
+    print("1. Rock")
+    print("2. Paper")
+    print("3. Scissors")
+    player2Choice = input("What would you like to chose ")
+
+    if int(player1Choice) == 1:
+        if int(player2Choice) == 1:
+            result = 2
+        elif int(player2Choice) == 2:
+            result = 1
+        elif int(player2Choice) == 2:
+            result = 3
+
+    elif int(player1Choice) == 2:
+        if int(player2Choice) == 1:
+            result = 3
+        elif int(player2Choice) == 2:
+            result = 2
+        elif int(player2Choice) == 2:
+            result = 1
+
+    elif int(player1Choice) == 3:
+        if int(player2Choice) == 1:
+            result = 1
+        elif int(player2Choice) == 2:
+            result = 3
+        elif int(player2Choice) == 2:
+            result = 2
+    os.system('cls')
+    if int(player1Choice) == 1:
+        print("Player 1 Chose: Rock") 
+    elif int(player1Choice) == 2:
+        print("Player 1 Chose: Paper")
+    elif int(player1Choice) == 3:
+        print("Player 1 Chose: Scissors") 
+    
+    if int(player2Choice) == 1:
+        print("Player 2 Chose: Rock") 
+    elif int(player2Choice) == 2:
+        print("Player 2 Chose: Paper")
+    elif int(player2Choice) == 3:
+        print("Player 2 Chose: Scissors")
+    
+    if int(result) == 2:
+        print("")
+        print("  _____  _                         __  __          ___           ")
+        print(" |  __ \| |                       /_ | \ \        / (_)          ")
+        print(" | |__) | | __ _ _   _  ___ _ __   | |  \ \  /\  / / _ _ __  ___ ")
+        print(" |  ___/| |/ _` | | | |/ _ \ '__|  | |   \ \/  \/ / | | '_ \/ __|")
+        print(" | |    | | (_| | |_| |  __/ |     | |    \  /\  /  | | | | \__ \ ")
+        print(" |_|    |_|\__,_|\__, |\___|_|     |_|     \/  \/   |_|_| |_|___/")
+        print("                  __/ |                                          ")
+        print("                 |___/                                           ")
+    elif int(result) == 2:
+        print("  ______ ")
+        print(" |______|")
+        print("  ______ ")
+        print(" |______|")
+        print("")
+        print("That was a tie")
+    elif int(result) == 1:
+        print("  _____  _                         ___   __          ___           ")
+        print(" |  __ \| |                       |__ \  \ \        / (_)          ")
+        print(" | |__) | | __ _ _   _  ___ _ __     ) |  \ \  /\  / / _ _ __  ___ ")
+        print(" |  ___/| |/ _` | | | |/ _ \ '__|   / /    \ \/  \/ / | | '_ \/ __|")
+        print(" | |    | | (_| | |_| |  __/ |     / /_     \  /\  /  | | | | \__ \ ")
+        print(" |_|    |_|\__,_|\__, |\___|_|    |____|     \/  \/   |_|_| |_|___/")
+        print("                  __/ |                                            ")
+        print("                 |___/                                             ")
+
+null = input("Press enter to exit")
